f7d293beb350638d844b21e11f4da26366c38391
Sina Samangooei <sinjax@gmail.com>
2011-06-03 16:11:07 +0000

    image terrier web service stuff

--- ImageTerrierWeb/grails-app/conf/DataSource.groovy
+++ ImageTerrierWeb/grails-app/conf/DataSource.groovy
@@ -26,7 +26,7 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+            url = "jdbc:hsqldb:file:ImageTerrierWeb-prodDb;shutdown=true"
         }
     }
 }
